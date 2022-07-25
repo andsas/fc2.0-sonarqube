ab531709366368fab7972f33b98b1271bb309de5
Andre santos <m1andsas@gmail.com>
2022-07-25 16:38:46 -0300

    ci: update test file 002

--- sum_test.go
+++ sum_test.go
@@ -3,9 +3,33 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	result := sum(2, 3)
+	result := sum(36, 12)
 
-	if result != 5 {
-		t.Error("The result must be 5")
+	if result != 48 {
+		t.Error("The result must be 48")
+	}
+}
+
+func TestSub(t *testing.T) {
+	result := sub(36, 12)
+
+	if result != 24 {
+		t.Error("The result must be 24")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := times(36, 12)
+
+	if result != 432 {
+		t.Error("The result must be 432")
+	}
+}
+
+func TestDiv(t *testing.T) {
+	result := div(36, 12)
+
+	if result != 3 {
+		t.Error("The result must be 3")
 	}
 }
