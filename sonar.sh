ab4a2ebcaf057cf7b34276dd4bcfcad766f1960e
Andre santos <m1andsas@gmail.com>
2022-07-25 14:57:13 -0300

    feat: modify file of sonar execution

--- sonar.sh
+++ sonar.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -e
 
+# ....................................................................................................
+# Error Report
+
+function error_report()
+{
+    echo "[$(date '+%H:%M:%S')] - [ERROR]: Script execution failed on line ${1}" >&2
+}
+
+trap "error_report ${LINENO}" ERR
+
+# ....................................................................................................
+# Executions
+
 go test -coverprofile=coverage.out
 docker container run --volume $(pwd):/usr/src --rm sonarsource/sonar-scanner-cli:4 sonar-scanner
+
+# ....................................................................................................
+# End Script
